fb998d5c8bb850cd8960a6035274719be650b210
LogFlames <elilun03@gmail.com>
2020-06-16 15:33:48 +0200

    Add shiny app, allow file select, allow column choice

---
+++ R/app.R
@@ -0,0 +1,135 @@
+library("shiny")
+library("zeallot")
+
+# The standard locus names, mostly for faster testing
+locus_names <- c("G10L", "G10L.1", "Mu05", "Mu05.1", "Mu09", "Mu09.1", "Mu10", "Mu10.1",
+                 "Mu23", "Mu23.1", "Mu50", "Mu50.1", "Mu51", "Mu51.1", "Mu59", "Mu59.1")
+
+# Define UI
+ui <- fluidPage(
+
+  # App title
+  titlePanel("Match Genotype"),
+
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
+
+    # Sidebar panel for inputs
+    sidebarPanel(
+
+      # Input: Select a file
+      fileInput("file1", "Choose CSV File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+
+
+      # Input: Checkbox if file has header
+      checkboxInput("header", "Header", TRUE),
+
+      tags$hr(),
+      # Select allele mismatch value
+      textInput(inputId = "alleleMismatchValue", label = "Allowed Allele-mismatch", value = "3"),
+
+      h4("Type the column name of the specified columns."),
+      h5("If header is deseleted, type the indexes of the columns."),
+
+
+      # Select Index Column
+      textInput(inputId = "indexColumnName", label = "Index Column", value = "SEP"),
+
+      # All user to specify colmn-names/indexes for
+      textInput(inputId = "dateColumnName", label = "Date Column", value = "Funnetdatum"),
+      textInput(inputId = "northColumnName", label = "North Column", value = "Nord"),
+      textInput(inputId = "eastColumnName", label = "East Column", value = "Ost"),
+      textInput(inputId = "genderColumnName", label = "Gender Column", value = "Kon"),
+      textInput(inputId = "presetIndColumnName", label = "PresetIndividual Column (DOES NOTHING ATM)", value = ""),
+
+      # Select Locus Columns
+      textInput(inputId = "locusColumnNames", label = "Locus Columns (separated by ',')", value = paste0(locus_names, collapse = "", sep = ",")),
+
+      # Parse Data
+      actionButton(inputId = "groupIndividuals", label = "GROUP INDIVIDUALS"),
+
+      tags$hr(),
+      tags$hr(),
+
+      # Display some result data to the user, TODO: allow the user to handle these exceptations
+      textOutput(outputId = "amtMultipleMatches"),
+      textOutput(outputId = "amtUnclassified"),
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: Data file ----
+      tableOutput("contents")
+
+    )
+
+  )
+)
+
+# Define server logic to read selected file ----
+server <- function(input, output, session) {
+  # Define defaults, unnecessary as they get defined by the default values from the ui
+  additional_data <- list(date="Funnetdatum", north="Nord", east="Ost", gender="Kon", preset_ind="")
+
+  # Define empty lists for all the data objects
+  search_data <- list(index = character(), multilocus = character(), individ_id = character())
+  multiple_matches <- list(index = character())
+  unclassified <- list(index = character(), multilocus = character())
+
+  # Run allelematch and all GenotypeChecks the surrounding code when the click of the button
+  observeEvent(input$groupIndividuals, {
+    groupIndividuals()
+  })
+
+  groupIndividuals <- function() {
+    # Render the table of all sample data
+    output$contents <- renderTable({
+      req(input$file1)
+
+      # Read the locus data from the ui
+      locus_columns <- strsplit(input$locusColumnNames, ",")[[1]]
+
+      # Read all of columns for the additional data from the ui
+      additional_data <<- list(date = input$dateColumnName, north = input$northColumnName, east = input$eastColumnName, gender = input$genderColumnName, preset_ind = input$presetIndColumnName)
+      # If the user does not specify column they get removed here to not have empty objects later that mess things up
+      additional_data <<- additional_data[is.na(additional_data)]
+
+      index_column <- input$indexColumnName
+
+      # Convert the numbers if header is deselected (and we are handeling columnindexes instead of columnnames)
+      if (!input$header) {
+        locus_columns <- sapply(locus_columns, as.numeric)
+        additional_data <<- sapply(additional_data, as.numeric)
+        index_column <- as.numeric(index_column)
+      }
+
+      # Unpack the different data returned by our wrapper of allelematch into temp variables
+      c(search_data_temp, multiple_matches_temp, unclassified_temp) %<-% create_search_data(input$file1$datapath, index_column, additional_data, locus_columns, as.numeric(input$alleleMismatchValue))
+
+      # Change the session (server) data from the temp data
+      search_data <<- search_data_temp
+      multiple_matches <<- multiple_matches_temp
+      unclassified <<- unclassified_temp
+
+      # Show the indexes, multilocus and individual id data to the user
+      return(search_data)
+    })
+
+    # Display the amount of problematic data. TODO: Allow user to handle this data and tell the program what to do
+    output$amtMultipleMatches <- renderText(
+      paste0("There were: ", length(multiple_matches), " samples that matched multiple individuals.")
+    )
+
+    output$amtUnclassified <- renderText(
+      paste0("There were: ", length(unclassified$index), " samples that were unclassified.")
+    )
+  }
+}
+
+shinyApp(ui = ui, server = server)
--- R/import_data.R
+++ R/import_data.R
@@ -1,71 +1,106 @@
+library(tidyverse)
+library(allelematch)
+
 #' Import and format data
+#' @description To be replaced by the user chosing the relevant columns
 #'
 #' @param file The path to the file to be imported
+#' @param index_column The name or index of the column containing the indexes
+#' @param additional_data A vector with the names or indexes to the columns that contain the date, north, east, gender, and any preset id:s in that order.
+#' @param locus_names A vector with the name or indexes to the columns that contain the genotypes
 #'
 #' @return A table with the relevant columns from the file
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' data <- import_data("raw_data.csv")
-import_data <- function(file) {
-  raw_data <- read.csv(file = file, header = TRUE, sep = ",", na.strings = c("NA"))
-  multilocus_names <- c("G10L", "G10L.1", "Mu05", "Mu05.1", "Mu09", "Mu09.1", "Mu10", "Mu10.1",
-                        "Mu23", "Mu23.1", "Mu50", "Mu50.1", "Mu51", "Mu51.1", "Mu59", "Mu59.1")
+#' }
+import_data <- function(file, index_column, additional_data, locus_names) {
+  # Read the data from the file
+  raw_data <- read.table(file = file, header = TRUE, sep = ",", na.strings = c("NA"))
+  # Select only the columns we are intressted in
   data <- raw_data %>%
-    select("SEP", "Funnetdatum", "Nord", "Ost", "Kon", "Individ", all_of(multilocus_names))
+    select(all_of(index_column), as.vector(unlist(additional_data)), all_of(locus_names))
+  # Return the resulting table
   data
 }
 
-#' Convert the data to a allelematch dataset
+
+#' Convert The Data to a Allelematch Dataset
+#' @description To be rewritten to take a table with only index and locus data, split in the \code{\link{import_data}} function.
+#' Now ignores the irrelevant data for this step, can be connected by their index later. A wrapper for the
+#' \code{\link{amDataset}} function.
 #'
 #' @param data The relevant data that has been imported
+#' @param index_column The name or indexe to the column that contain the index of the data
+#' @param ignore_columns A vector with the names or indexes to the columns that are to be ignored by the 'allelematch' packet. These are the date, nord, east, gender and preset individual, in that order. TODO: Remove the preset individual, it should be used earlier.
 #'
 #' @return A allelematch dataset with the relevant index and locus column
 #' @export
 #'
 #' @examples
-#' am_data <- get_allelematch_dataset(data)
-get_allelematch_dataset <- function(data) {
-  am_data <- amDataset(data, indexColumn = "SEP", ignoreColumn = c("Funnetdatum", "Nord", "Ost", "Kon", "Individ"), missingCode = "NA")
+#' \dontrun{
+#' am_data <- create_allelematch_dataset(data)
+#' }
+create_allelematch_dataset <- function(data, index_column, ignore_columns) {
+  # Create the allelematch dataset
+  am_data <- allelematch::amDataset(data, indexColumn = index_column, ignoreColumn = as.vector(unlist(ignore_columns)), missingCode = "000")
+  # Retrun the allelematch dataset
   am_data
 }
 
-#' TODO:
-#' - Check what the diffrences are between calculated data and given data
-#' - Match for exact match
-#' - Give close
+#' Load Data and Group Into Individuals
+#' @description TODO: Keep the multimatch data
+#'
+#' @param file_path A path to the csv file
+#' @param index_column The name or index to the colmn containing the indexes for the data
+#' @param additional_data_columns A named list with the names or indexes to the date, north, east, gender and preset_ind colmns.
+#' @param locus_columns A vector with the names or indexes to the columns containing the locus data
+#' @param allele_mismatch A value for how many allele mismatchs are to be allowed and still count like a match
+#'
+#' @return A list with $index, $multilocus, and $individ_id.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' search_data <- create_search_data("data.csv")
+#' }
+create_search_data <- function(file_path, index_column, additional_data_columns, locus_columns, allele_mismatch) {
+  data <- import_data(file_path, index_column = index_column, additional_data = additional_data_columns, locus_names = locus_columns)
+  am_data <- create_allelematch_dataset(data, index_column = index_column, ignore_columns = additional_data_columns)
 
-library(tidyverse)
-library(allelematch)
+  am_unique <- amUnique(am_data, alleleMismatch = allele_mismatch)
 
-data <- import_data("R/AC_allKorr.csv")
-am_data <- get_allelematch_dataset(data)
+  ind <- 0
+  search_data <- data.frame(index = character(), multilocus = character(), individ_id = character())
 
-alleleMismatch <- 3
-am_unique <- amUnique(am_data, alleleMismatch = alleleMismatch)
+  for (pair in am_unique$pairwise) {
+    multilocus_combined <- apply(pair$match$multilocus, 1, combine_multilocus)
 
-indexes <- c()
-individ_id <- c()
-multilocuses <- c()
-ind <- 0
+    search_data <- rbind(search_data, list(index = pair$match$index, multilocus = multilocus_combined, individ_id = rep(as.character(ind), length(pair$match$index))))
+    ind <- ind + 1
+  }
 
-search_data <- data.frame()
-
-for (pair in am_unique$pairwise) {
-  multilocus <- paste0(unname(split(pair$match$multilocus, rep(1:nrow(pair$match$multilocus), ncol(pair$match$multilocus)))))
-
-
-  search_data <- search_data %>%
-    rbind(list(index = pair$match$index, multilocus = pair$match$multilocus))
-  individ_id <- individ_id %>%
-    c(rep(ind, length(pair$match$index)))
-  ind <- ind + 1
-  indexes <- indexes %>%
-    c(pair$match$index)
-  multilocuses <- multilocuses %>%
-    c(unname(split(pair$match$multilocus, rep(1:nrow(pair$match$multilocus), ncol(pair$match$multilocus)))))
+  list(search_data, am_unique$multipleMatches$index, list(index = am_unique$unclassified$index, multilocus = am_unique$unclassified$multilocus))
 }
 
-search_data <- list(index = indexes, multilocus = multilocuses, individ_id = individ_id)
-
-write.csv(search_data, file = "R/out.csv", sep = ",")
+#' Combine Multiple Locus and Assure Constant Width
+#'
+#' @param locus A vector with all the locus in string format.
+#'
+#' @return A long string with all locus combined, padding zeros making ever locus three characters long.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' multilocus <- c("182", "180", "152", "152")
+#' multilocus_combined <- combine_multilocus(multilocus)
+#' }
+#' \dontrun{
+#' multilocus_combined <- apply(multilocus_matrix, 1, combine_multilocus)
+#' }
+#'
+combine_multilocus <- function(locus) {
+  paste0(formatC(as.numeric(locus), width = 3, flag = "0", format = "d"), collapse = "")
+}
