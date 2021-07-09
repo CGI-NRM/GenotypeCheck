ad2a7da08e8f53f97f46f89f02ac42e4df892ce5
Elias Lundell <elilun03@gmail.com>
2021-07-09 14:27:37 +0200

    Fix missing : that caused crash

--- R/app.R
+++ R/app.R
@@ -556,7 +556,7 @@ server <- function(input, output, session) {
         } else if (identical(input$distance_function, "man")) {
             distance_function <- GenotypeCheck::dist_manhattan
         } else if (identical(input$distance_function, "max")) {
-            distance_function <- GenotypeCheck:dist_maximum
+            distance_function <- GenotypeCheck::dist_maximum
         } else if (identical(input$distance_function, "num")) {
             distance_function <- GenotypeCheck::dist_num_mismatches
         }
